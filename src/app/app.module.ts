1ac4d3e15c62e5edaa3e6ae120f6c1d0aaf9c7aa
vaibhav19j <88232936+vaibhav19j@users.noreply.github.com>
2022-02-20 18:21:52 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbChatModule, NbSpinnerModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatbotComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbChatModule,
+    NbSpinnerModule,
+    HttpClientModule
+  ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/chatbot'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/chatbot/chatbot.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {webSocket} from 'rxjs/webSocket';
+
+const dialogflowURL = 'https://us-central1-fireship-lessons.cloudfunctions.net/dialogflowGateway';
+
+@Component({
+  selector: 'app-chatbot',
+  templateUrl: './chatbot.component.html',
+  styleUrls: ['./chatbot.component.scss']
+})
+export class ChatbotComponent implements OnInit {
+
+  messages = [];
+  loading = false;
+
+  // Random ID to maintain session with server
+  private subject = webSocket('ws://localhost:8080/push');
+
+  ngOnInit(): void {
+    this.subject.next('initial'); // <- ping first message
+    this.subject.subscribe(message => {       // <- listen messages from server
+      this.addBotMessage(message);
+    
+    });  
+}
+
+  
+
+  handleUserMessage(event) {
+    console.log(event);
+    const text = event.message;
+    this.addUserMessage(text);
+
+    // this.loading = true;
+
+    // Make an HTTP Request
+   this.subject.next(text);
+  }
+
+
+  // Helpers
+
+  addUserMessage(text) {
+    this.messages.push({
+      text,
+      sender: 'You',
+      reply: true,
+      date: new Date()
+    });
+  }
+
+  addBotMessage(text) {
+    this.messages.push({
+      text,
+      sender: 'Bot',
+      avatar: 'assets/bot.jpeg',
+      date: new Date()
+    });
+  }
+
+}
